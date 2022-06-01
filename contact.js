831a3d59d3a2967aa53f9d9fed12a74927bef9fd
nageswarao123 <106669355+nageswarao123@users.noreply.github.com>
2022-06-01 21:10:07 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact (){
+return <address>
+			You can find us here:<br />
+			SqubeSoftsol<br />
+			5th & 6th Floor, Arundlapet, A- 118, <br />
+			Sector- 136, Guntur, Andhra pradesh 
+		</address>
+}
+
+export default Contact;
